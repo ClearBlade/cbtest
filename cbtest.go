7408f68b32d3728c7d961ba780d0842ca37adf2e
Daniel Rivas <drivas@clearblade.com>
2020-11-12 13:40:02 -0600

    Added keep flag

--- cbtest.go
+++ cbtest.go
@@ -7,15 +7,17 @@ import (
 var (
 	// Flags, registered during init.
 	flagConfig *string
+	flagKeep   *bool
 )
 
 // init registers our flags.
 func init() {
 	// NOTE: reminder that flag.Parse will be called by `go test`, so we don't need to call it here.
 	flagConfig = flag.String("cbtest.config", "cbtest.yml", "Path to the config file to use (credentials mostly)")
+	flagKeep = flag.Bool("cbtest.keep", false, "Keep systems that were created by cbtest (external systems are never destroyed)")
 }
 
-// ConfigPath the value given to the `-cbtest.config` flag.
+// ConfigPath returns the value given to the `-cbtest.config` flag.
 func ConfigPath() string {
 
 	if flagConfig == nil {
@@ -23,8 +25,22 @@ func ConfigPath() string {
 	}
 
 	if !flag.Parsed() {
-		panic("ConfigPath called flag.Parse")
+		panic("ConfigPath called before flag.Parse")
 	}
 
 	return *flagConfig
+}
+
+// ShouldKeepSystem returns the value given to the `-cbtest.keep` flag.
+func ShouldKeepSystem() bool {
+
+	if flagKeep == nil {
+		panic("ShouldKeepSystem called before init")
+	}
+
+	if !flag.Parsed() {
+		panic("ShouldKeepSystem called before flag.Parse")
+	}
+
+	return *flagKeep
 }

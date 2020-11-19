4a0b30185fdf58bec526d1ec51252f024201f104
Daniel Rivas <drivas@clearblade.com>
2020-11-18 19:09:53 -0600

    Comments in streaming_test/message.go

--- examples/streaming_test/message.go
+++ examples/streaming_test/message.go
@@ -6,11 +6,13 @@ import (
 	"github.com/brianvoe/gofakeit/v5"
 )
 
+// Message to be published.
 type Message struct {
 	Timestamp int64  `json:"timestamp"`
 	Message   string `json:"message"`
 }
 
+// GenerateMessage generates a message with current timestamp and random phrase.
 func GenerateMessage() *Message {
 	return &Message{
 		Timestamp: time.Now().Unix(),

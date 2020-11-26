669863c53461d5f1359d090f7a19fa943cc1f561
Daniel Rivas <drivas@clearblade.com>
2020-11-26 16:34:12 -0600

    Added more verify tests

--- modules/check/verify_test.go
+++ modules/check/verify_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/onsi/gomega"
+	"github.com/stretchr/testify/mock"
 
 	"github.com/clearblade/cbtest/mocks"
 	"github.com/clearblade/cbtest/modules/check"
@@ -13,10 +14,15 @@ var testingT = &mocks.T{}
 
 func init() {
 	testingT.On("Helper").Return()
+	testingT.On("Errorf", mock.Anything, mock.Anything).Return()
 }
 
 func ExampleVerify_withGomega() {
 	fmt.Println(check.VerifyE(testingT, 10, gomega.BeNumerically(">", 5)))
+	fmt.Println(check.VerifyE(testingT, 10, gomega.BeNumerically("==", 10)))
+	fmt.Println(check.VerifyE(testingT, 10, gomega.BeNumerically(">", 15)))
 	// Output:
 	// true
+	// true
+	// false
 }

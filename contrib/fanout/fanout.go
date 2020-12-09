15491fe6f5a7b5aa047ef5cbe313672e51109f4c
Daniel Rivas <drivas@clearblade.com>
2020-12-09 17:28:03 -0600

    Removed fanout module

--- contrib/fanout/fanout.go
+++
@@ -1,8 +0,0 @@
-// Package fanout provides helpers for running and waiting for "worker" routines
-// not managed by parent testing framework.
-//
-// Why do we need this?
-//
-// The idea is to provide simple helper functions for spinning up many workers
-// in parallel for testing purposes (consumers, producers, etc).
-package fanout
